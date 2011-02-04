70b7930adab70b0e3f2d1f79ebad62fc2d85fa78
amyciav <amyciav@04565fab-d644-2a25-5f62-5d3a877184e2>
2011-02-04 17:24:53 +0000

    Removed half-wrong copy of P2P version.

--- trunk/rurple1.1-v_P2P/move.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ -z "$1" ]
-then
-    echo "Usage: username. Example Users/johndoe/.config. Enter the johndoe."
-    exit -1
-fi
-cp world_files/problem* /Users/$1/.config/rurple/worlds/samples/.
-rm /Users/$1/.config/rurple/worlds/samples/*~
