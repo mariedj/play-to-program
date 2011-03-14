64d69cd34022e3d1a1af89f79fe52d2f5407e5b0
rtdeloatch <rtdeloatch@04565fab-d644-2a25-5f62-5d3a877184e2>
2011-03-14 06:39:25 +0000

    Added student.py, problems.py, Updated rur_start.py for post test and saving of data

---
+++ rurple1.1-P2P/rur_py/problems.py
@@ -0,0 +1,43 @@
+def problem1():
+    return ('problem1.wld', 'prob1.htm')
+problem1.concepts = ["IA1","IB2a"]
+problem1.difficulty = 8
+
+def problem2():
+    return ('problem2.wld', 'prob2.htm')
+problem2.concepts = ["IA1","IB2a"]
+problem2.difficulty = 8
+
+def problem3():
+    return ('problem3.wld', 'prob3.htm')
+problem3.concepts = ["IA1","IB2a"]
+problem3.difficulty = 8
+
+def problem4():
+    return ('problem4.wld', 'prob4.htm')
+problem4.concepts = ["IA1","IB2a"]
+problem4.difficulty = 8
+
+def problem5():
+    return ('problem5.wld', 'prob5.htm')
+problem5.concepts = ["IA1","IB2a"]
+problem5.difficulty = 8
+
+def problem6():
+    return ('problem6.wld', 'prob6.htm')
+problem6.concepts = ["IA1","IB2a"]
+problem6.difficulty = 8
+
+def problem7():
+    return ('problem7.wld', 'prob7.htm')
+problem7.concepts = ["IA1","IB2a"]
+problem7.difficulty = 8
+
+def problem8():
+    return ('problem8.wld', 'prob8.htm')
+problem8.concepts = ["IA1","IB2a"]
+problem8.difficulty = 8
+
+
+writing = (problem1,problem2,problem3,problem4,problem5,problem6,problem7,problem8)
+
