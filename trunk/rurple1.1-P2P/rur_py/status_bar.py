a39efe1d5f328315607439bc88c51b636481bd6b
luigiwalser@gmail.com <luigiwalser@gmail.com@04565fab-d644-2a25-5f62-5d3a877184e2>
2011-03-15 21:00:18 +0000

    Fix one more thing SVN messed up

--- trunk/rurple1.1-P2P/rur_py/status_bar.py
+++ trunk/rurple1.1-P2P/rur_py/status_bar.py
@@ -22,38 +22,34 @@ class rurStatusBar(wx.StatusBar):
         self.problem_field = 0
         self.running_field = 1
         self.beeper_field = 2
-        self.world_field = 3
-        self.user_field = 4
+        self.user_field = 3
 
         # Initial values for the fields
         self.prob_text = _("No problem loaded")
         self.run_text = _("Program not running")
         self.beeper_text = _("Robot has %s beeper")%0
-        self.world_text = _("Default world")
-        self.user_text = _("Default User")
+        self.user_text = _("Default user")
 
         #=== when changing language
         self.running_dict = {'0': _("Program not running"),
                              '1': _("Program is running"),
                              '2':  _("Program paused")}
-        self.fields_info = { 'problem': '', 'running': '0', 'user':'',
-                             'beepers' : 0, 'world': '', 'robot': 1}
+        self.fields_info = { 'problem': '', 'running': '0',
+                             'beepers' : 0, 'user': '', 'robot': 1}
 
         #===================================
 
         self.notebook_new_page = 99
 
     def ClearFields(self):
-        for field in range(5):
+        for field in range(4):
             self.SetStatusText('', field)
 
     def WriteFields(self):
         self.SetStatusText(self.prob_text, self.problem_field)
         self.SetStatusText(self.run_text, self.running_field)
         self.SetStatusText(self.beeper_text, self.beeper_field)
-        self.SetStatusText(self.world_text, self.world_field)
         self.SetStatusText(self.user_text, self.user_field)
-
 
     def UpdateFields(self, *args):
         field, info = args[0].data[0]
@@ -77,14 +73,37 @@ class rurStatusBar(wx.StatusBar):
             for n in ['0', '1', '2']:
                 if info == self.running_dict[n]:
                     self.fields_info['running'] = n
-        elif field == self.world_field:
-            self.world_text = _("World file: %s") % info
-            self.fields_info['world'] = info
-        elif field == self.problem_field:
-            self.prob_text = _("Problem: %s") % info
-            self.fields_info['problem'] = info
         elif field == self.user_field:
             self.user_text = _("User ID: %s") % info
             self.fields_info['user'] = info
+        elif field == self.problem_field:
+            self.prob_text = _("Problem: %s") % info
+            self.fields_info['problem'] = info
         self.WriteFields()
+
+    def ChangeLanguage(self):
+        # update strings in dict
+        self.running_dict = {'0': _("Program not running"),
+                             '1': _("Program is running"),
+                             '2':  _("Program paused")}
+        # problem
+        if self.fields_info['problem'] != '':
+            self.prog_text = _("Problem: %s") % self.fields_info['problem']
+        else:
+            self.prog_text = _("No problem loaded")
+        # running status
+        self.run_text = self.running_dict[self.fields_info['running']]
+        # beepers carried
+        if self.fields_info['robot'] == self.no_robot:
+            self.beeper_text = _("No robot in this world")
+        elif self.fields_info['beepers'] < 2:
+            self.beeper_text = _("Robot has %s beeper") % self.fields_info['beepers']
+        else:
+            self.beeper_text = _("Robot has %s beepers") % self.fields_info['beepers']
+        # user ID/name
+        if self.fields_info['user'] != '':
+            self.user_text = _("User ID: %s") % self.fields_info['user']
+        else:
+            self.user_text = _("Default user")
+        # now, ready to update when robot notebook page is selected
 
