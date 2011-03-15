ee750d9d31d65a0e374e95a1a8e6f7c13691979b
luigiwalser@gmail.com <luigiwalser@gmail.com@04565fab-d644-2a25-5f62-5d3a877184e2>
2011-03-15 20:56:30 +0000

    Update problem meta-data

--- trunk/rurple1.1-P2P/rur_py/problems.py
+++ trunk/rurple1.1-P2P/rur_py/problems.py
@@ -1,42 +1,42 @@
 def problem1():
     return ('problem1.wld', 'prob1.htm')
-problem1.concepts = ["IA1","IB2a"]
-problem1.difficulty = 2
+problem1.concepts = ["IB2a","IB2c","IB3","IIIA2"]
+problem1.difficulty = 3
 
 def problem2():
     return ('problem2.wld', 'prob2.htm')
-problem2.concepts = ["IA1","IB2a"]
-problem2.difficulty = 5
+problem2.concepts = ["IB1a","IB3","IIA1","IIA2","IIIA1","IIIA2"]
+problem2.difficulty = 6
 
 def problem3():
     return ('problem3.wld', 'prob3.htm')
-problem3.concepts = ["IA1","IB2a"]
-problem3.difficulty = 5
+problem3.concepts = ["IA1","IA2","IB2a","IB3"]
+problem3.difficulty = 2
 
 def problem4():
     return ('problem4.wld', 'prob4.htm')
-problem4.concepts = ["IA1","IB2a"]
-problem4.difficulty = 2
+problem4.concepts = ["IA2","IB2a","IB3"]
+problem4.difficulty = 3
 
 def problem5():
     return ('problem5.wld', 'prob5.htm')
-problem5.concepts = ["IA1","IB2a"]
-problem5.difficulty = 8
+problem5.concepts = ["IB2a"]
+problem5.difficulty = 2
 
 def problem6():
     return ('problem6.wld', 'prob6.htm')
-problem6.concepts = ["IA1","IB2a"]
-problem6.difficulty = 8
+problem6.concepts = ["IA1","IA2","IA3","IB2a","IB3","IIIA2"]
+problem6.difficulty = 7
 
 def problem7():
     return ('problem7.wld', 'prob7.htm')
-problem7.concepts = ["IA1","IB2a"]
-problem7.difficulty = 8
+problem7.concepts = ["IA1","IB1a","IB1b","IB3","IIIA2"]
+problem7.difficulty = 4
 
 def problem8():
     return ('problem8.wld', 'prob8.htm')
 problem8.concepts = ["IA1","IB2a"]
-problem8.difficulty = 8
+problem8.difficulty = 1
 
 
 writing = (problem1,problem2,problem3,problem4,problem5,problem6,problem7,problem8)
