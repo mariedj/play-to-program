e37504eea4146d03dedf27ecbea1e0cc38895203
tuzoj1 <tuzoj1@umbc.edu>
2011-07-19 01:44:27 +0000

    Added "soft" student models, changed error in constructor

--- student_model/students.py
+++ student_model/students.py
@@ -6,7 +6,7 @@ import problem
 class Student:
     ''' Defines a model for a virtual student
     '''
-    def __init__(self, num_concepts):
+    def __init__(self, concepts):
         self.concepts = concepts
         self.num_concepts = len(concepts)
         self.competences = {}
@@ -55,7 +55,30 @@ class LogisticStudent(Student):
     def logistic_fn(self, problemDifficulty, competence):
         return 1.0 / (1 + math.exp(-16 * (competence + .1 - problemDifficulty)))
 
+        
+class SoftLogisticStudent(LogisticStudent):
+    ''' "Soft" logistic student model.
+    
+    Same as the logistic student model, but with upper and lower boundaries at
+    0.9 and 0.1, respectively, rather than 1.0 and 0.0. This corresponds with
+    the chance that a student answers a problem correctly or incorrectly by
+    accident.
+    '''
 
+    def get_prob_correct(self, problem):
+        correct = []
+        for concept in self.concepts:
+            if problem.get_difficulty(concept) != 0:
+                likelihood = self.logistic_fn(problem.get_difficulty(concept),
+                    self.get_competence(concept))
+                correct.[concept] = likelihood
+
+        return correct
+
+    def logistic_fn(self, problemDifficulty, competence):
+        return 0.1 + 0.8 / (1 + math.exp(-16 * (competence + .1 - problemDifficulty)))
+
+        
 class LinearStudent(Student):
     ''' Linear student model.
 
@@ -75,6 +98,29 @@ class LinearStudent(Student):
         return correct
 
 
+class SoftLinearStudent(Student):
+    ''' "Soft" linear student model.
+
+    Success probability is determined by a linear function. Upper and lower
+    bounds of the function are 0.9 and 0.1, respectively, to reflect the chance
+    that a student will answer a question correctly or incorrectly by
+    accident.
+    '''
+    
+    def get_prob_correct(self, problem):
+        correct = []
+        for concept in range(self.num_concepts):
+            if self.get_competence(concept) != 1:
+                likelihood = (problem.get_difficulty(concept) - 1) / \
+                              (self.get_competence(concept) - 1)
+                if likelihood > 0.9:
+                    likelihood = 0.9 # cap high at 0.9
+                if likelihood < 0.1:
+                    likelihood = 0.1 # cap low at 0.1
+                correct[concept] = likelihood
+        return correct
+        
+        
 class BinaryStudent(Student):
     ''' Binary student model.
     
@@ -89,9 +135,21 @@ class BinaryStudent(Student):
         correct = {}
         for concept in self.concepts:
             if self.get_competence(concept) < problem.get_difficulty(concept):
-                correct[concept] = 0.1
+                correct[concept] = 0.0
             elif problem.get_difficulty(concept) > 0:
-                correct[concept] = .9
+                correct[concept] = 1.0
         
         return correct
     
+    
+class SoftBinaryStudent(Student):
+
+    def get_prob_correct(self, problem):
+        correct = []
+        for concept in range(self.num_concepts):
+            if self.get_competence(concept) < problem.get_difficulty(concept):
+                correct[concept] = 0.1
+            elif problem.get_difficulty(concept) > 0:
+                correct[concept] = 0.9
+        
+        return correct
