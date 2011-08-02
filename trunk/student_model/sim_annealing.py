7f254a294409208537e36506c264b6cdadea6640
luigiwalser@gmail.com <luigiwalser@gmail.com@04565fab-d644-2a25-5f62-5d3a877184e2>
2011-08-02 16:12:48 +0000

    move simulated annealing code to new file

--- trunk/student_model/sim_annealing.py
+++ trunk/student_model/sim_annealing.py
@@ -9,51 +9,45 @@ def mkdict(concepts, values):
     return ans
 
 def most_likely_explanation(test_student, problem_set, student_answers, calculator):
-    guess = []
-    num_concepts =  test_student.num_concepts
-    for i in range(num_concepts):
-        guess.append(.5)
+    for concept in test_student.concepts:
+        test_student.set_competence(concept, 0.5)
+        
+    simulated_annealing(test_student, problem_set, student_answers, calculator)
     
-    test_student.competences = mkdict(test_student.concepts, guess)
-    
-    guess = simulated_annealing(guess, test_student, problem_set, student_answers, calculator)
-    
-    return mkdict(test_student.concepts, guess)
+    return test_student.competences
 
-def simulated_annealing(guess, test_student, problem_set, student_answers, calculator):
-    s = guess
-    test_student.competences= mkdict(test_student.concepts, s)
-    e = students.get_probability_of_outcome(problem_set, student_answers, test_student, calculator)
-    sbest = []
-    for item in s:
-        sbest.append(item)
+def simulated_annealing(test_student, problem_set, student_answers, calculator):
+    s = test_student.competences
+    e = get_probability_of_outcome(problem_set, student_answers, test_student, calculator)
+    sbest = {}
+    for concept in s:
+        sbest[concept] = s[concept]
     ebest = e
     k = 0
     kmax = 2000.0
     while k < kmax:
         temp = k / kmax
         snew = gen_neighbor(s, temp)
-        test_student.competences= mkdict(test_student.concepts, snew)
+        test_student.competences = snew
         enew = students.get_probability_of_outcome(problem_set, student_answers, test_student, calculator)
-        
         if prob_move(e, enew, temp) > random.random():
             s = snew
             e = enew
         if e > ebest:
-            sbest = []
-            for item in s:
-                sbest.append(item)
+            sbest = {}
+            for concept in s:
+                sbest[concept] = s[concept]
             ebest = enew
         k += 1
-
-    return sbest
+    #return sbest
+    test_student.competences = sbest
 
 def gen_neighbor(guess, temperature):
-    neighbor = []
-    for competence in guess:
+    neighbor = {}
+    for concept in guess:
         move = random.randint(-1, 1)
-        new_comp = min(1, max(0, competence + temperature * move))
-        neighbor.append(new_comp)
+        new_comp = min(1, max(0, guess[concept] + temperature * move))
+        neighbor[concept] = new_comp
     return neighbor
         
 def prob_move(e, enew, t):
