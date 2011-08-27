e89cdb397aa4ed7b82716c7dfc7952a7eb89eee8
tuzoj1 <tuzoj1@umbc.edu>
2011-08-27 22:14:33 +0000

    Deleting hillclimber code

--- student_model/exam.py
+++ student_model/exam.py
@@ -30,12 +30,12 @@ class ProblemSet:
 class RandomProblemSet(ProblemSet):
     
     def __init__(self, concepts, num_problems=0, num_answers=-1, 
-    avg_concepts_involved=2):
+                 avg_concepts_involved=2):
         ProblemSet.__init__(self, concepts)
         self.addRandomProblems(num_problems, num_answers, avg_concepts_involved)
 
     def addRandomProblems(self, num_problems, num_answers=-1, 
-    avg_concepts_involved=2):
+                          avg_concepts_involved=2):
         for i in range(num_problems):
             self.problems.append(problem.RandomProblem(self.concepts, 
                 num_answers, avg_concepts_involved))
